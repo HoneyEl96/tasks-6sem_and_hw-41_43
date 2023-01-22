82a92990fd02f2c9e73520a0b570695b98cf99f4
Artyom M <milch96@ya.ru>
2023-01-22 23:37:36 +0300

    43 zadacha reshena, no otvet ne kak v primere

--- Program.cs
+++ Program.cs
@@ -185,3 +185,23 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите b1-координату"); 
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите k1-координату");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите b2-координату");
+int b2 = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите k2-координату"); 
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+// k1 * x + b1 = k2 * x + b2
+// (k1 - k2) * x = b2 - b1
+// x = (b2 - b1) / (k1 - k2)
+// y = k1 * x + b1
+
+int k3 = k1 - k2;
+int b3 = b2 - b1;
+double x = b3 / k3;
+double z = k1 * x;
+double y = z + b1;
+Console.WriteLine("X: " + Math.Pow(x , 1) + " " + "Y: " + Math.Pow(y , 1));
